ccca351e4a908860419c43efe06e8b32a7d99e5f
megakoresh <megakoresh@gmail.com>
2015-04-20 14:38:01 +0300

    Revert "Revert "Reset to pre-post commit. Will merge manually.""
    This reverts commit 3f61656f87ad4394d90cc790f348d260a5d56b3b.

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -5,19 +5,23 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
-module.exports = {
-  post: function(req, res) {
-    params = req.params.all();
-    params.userID = req.session.user;
-    Post.create(params).exec(function(err, post){
-      if(err) {
-        console.log(err);
-        return res.negotiate(err);
-      }
-      if(post) {
-        //socket.publish(post)???? How the fuck does this work?!
-      }
-    });
-  }
-};
+ module.exports = {
+   post: function(req, res) {
+     params = req.params.all();
+     params.userID = req.session.user;
+     Post.create(params).exec(function(err, post){
+       if(err) {
+         console.log(err);
+         return res.negotiate(err);
+       }
 
+       if(post){
+         //list of socket methods: https://gist.github.com/mikermcneil/6598661
+         var io = sails.io;
+         //so essentially this should only emit to followers
+         io.sockets.emit('message', {content: params['content'] });
+         return res.redirect('panel');
+       }
+     });
+   }
+ };
