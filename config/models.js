d4ac821f0564f062bc2ebe42c62e820100cf747a
Leo Tuppurainen <leo.tuppurainen@gmail.com>
2015-04-22 00:24:52 +0200

    Change database configuration

--- config/models.js
+++ config/models.js
@@ -17,7 +17,7 @@ module.exports.models = {
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
- connection: 'remoteSQL',//'localDiskDb',
+ connection: 'mongo',//'localDiskDb',
 
   /***************************************************************************
   *                                                                          *
